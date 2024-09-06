59fb1d059c11f907c29409b7278232b2b0956970
annjose-alpha <103119728+annjose-alpha@users.noreply.github.com>
2024-09-06 17:23:23 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -4,3 +4,19 @@ test("the best flavor is grapefruit", () => {
   expect(bestLaCroixFlavor()).toBe("grapefruit");
 });
 
+test("the user can post a message to their timeline", () => {
+  expect(bestLaCroixFlavor()).toBe("grapefruit");
+});
+
+test("the user can view a friend's timeline", () => {
+  expect(bestLaCroixFlavor()).toBe("grapefruit");
+});
+
+test("the user can follow a new friend and view list of subscriptions", () => {
+  expect(bestLaCroixFlavor()).toBe("grapefruit");
+});
+
+test("the user can view the posts on their wall", () => {
+  expect(bestLaCroixFlavor()).toBe("grapefruit");
+});
+
