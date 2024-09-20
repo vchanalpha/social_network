ddc312666115adb0289f5968b224a503fe23cd3a
Ann Jose <annjose@Anns-MBP.localdomain>
2024-09-20 13:59:17 +0200

    added tests for follow functionality

--- app.test.js
+++ app.test.js
@@ -14,8 +14,13 @@ test("the user can view a friend's timeline", () => {
   expect(bestLaCroixFlavor()).toBe("grapefruit");
 });
 
-test("the user can follow a new friend and view list of subscriptions", () => {
-  expect(bestLaCroixFlavor()).toBe("grapefruit");
+test("the user can follow a new friend", () => {
+  const network = new Network(testUser);
+  network.currentUser.follow(testUser2);
+  expect(network.currentUser.subscriptions).toHaveProperty(convertNameToId(testUser2));
+  network.currentUser.follow(testUser3);
+  expect(network.currentUser.subscriptions).toHaveProperty(convertNameToId(testUser3));
+  expect(Object.keys(network.currentUser.subscriptions).length).toBe(2);
 });
 
 test("the user can view the posts on their wall", () => {
